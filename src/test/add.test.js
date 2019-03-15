3bd2a62a669ca0a8e0afa0f0b7745529ef3fbd68
Miguel BEcerra <miguelbecerra01@gmail.com>
2019-03-15 15:47:12 -0300

    adding test jest packages

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -1,7 +1,16 @@
 const add = (a, b) => a + b;
-
+const generateGreeting = (name='Anonymous') => `Hello ${name}!`;
 
 test('should add two numbers', () => {
     const result = add(3, 4);
-    
+    expect(result).toBe(7);
 });
+
+test('should generate greeting from name', () =>{
+    const name = 'Miguel';
+    expect(generateGreeting(name)).toBe('Hello Miguel!');
+}); 
+
+test('should generate greeting with no name', () =>{
+    expect(generateGreeting()).toBe('Hello Anonymous!');
+}); 
